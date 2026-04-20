74d80b7f892ddbc4e3a83164f24c8e8531b68adb
sunus <sunusukesan96@gmail.com>
2026-04-21 01:14:12 +0530

    update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,8 @@ import axios from "axios"
 const API = axios.create({
 
 // baseURL: "http://localhost:5000/api"
-baseURL:import.meta.env.VITE_API_URL + "/api" ,
+// baseURL:import.meta.env.VITE_API_URL + "/api" ,
+baseURL: "https://expense-splitter-02tc.onrender.com/api"
 
 })
 
