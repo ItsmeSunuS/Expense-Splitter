48833fff7b263909addd1f3ccc016e1f1dfffbac
sunus <sunusukesan96@gmail.com>
2026-04-21 00:17:47 +0530

    update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -8,3 +8,14 @@ baseURL:import.meta.env.VITE_API_URL + "/api" ,
 })
 
 export default API
+
+// import axios from "axios";
+
+// const baseURL =
+//   import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+// const API = axios.create({
+//   baseURL: `${baseURL}/api`,
+// });
+
+// export default API;
